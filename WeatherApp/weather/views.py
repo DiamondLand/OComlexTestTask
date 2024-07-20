df9d06ae34785a20e96d063943241682eb916380
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2024-07-20 23:44:34 +0300

    City model & show cycle data

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,31 +1,38 @@
 import requests
 
 from django.shortcuts import render
+from .models import City
 
 
 def index(request):
     appid='89a0ebedfa3a05c7f1384f2de25ea2e6'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units=metric'
-    city = 'London'
 
-    weather_response = requests.get(url=url.format(city=city, appid=appid))
-    if weather_response.status_code != 200 or 'error' in weather_response.json():
-        weather_json = weather_response.json()
-        city_info = {
-            'city': 'Произошла ошибка',
-            'temp': None,
-            'icon': weather_json['error']
-        }
-    else:
-        weather_json = weather_response.json()
-        city_info = {
-            'city': city,
-            'temp': weather_json['main']['temp'],
-            'icon': weather_json['weather'][0]['icon']
-        }
+    cities = City.objects.all()
+    all_cities = []
+
+    for city in cities:
+        weather_response = requests.get(url=url.format(city=city.name, appid=appid))
+
+        if weather_response.status_code != 200 or 'error' in weather_response.json():
+            weather_json = weather_response.json()
+            city_info = {
+                'city': 'Произошла ошибка',
+                'temp': None,
+                'icon': weather_json['error']
+            }
+        else:
+            weather_json = weather_response.json()
+            city_info = {
+                'city': city.name,
+                'temp': weather_json['main']['temp'],
+                'icon': weather_json['weather'][0]['icon']
+            }
+    
+        all_cities.append(city_info)
 
     return render(
         request=request,
         template_name='weather/index.html',
-        context={'info': city_info}
+        context={'all_info': all_cities}
     )
