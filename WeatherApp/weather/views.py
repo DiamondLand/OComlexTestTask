1587982f4422323b5c85761436f82f7e805de100
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2024-07-20 23:24:47 +0300

    get & show static weather

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,7 +1,32 @@
+import requests
+
 from django.shortcuts import render
 
-def index(request) -> render:
+
+def index(request):
+    token='89a0ebedfa3a05c7f1384f2de25ea2e6'
+    appid='82b797b6ebc625032318e16f1b42c016'
+    url = 'https://samples.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
+    city = 'Moscow'
+
+    weather_response = requests.get(url=url.format(city=city, appid=appid))
+    if weather_response.status_code != 200 or 'error' in weather_response.json():
+        weather_json = weather_response.json()
+        city_info = {
+            'city': 'Произошла ошибка',
+            'temp': None,
+            'icon': weather_json['error']
+        }
+    else:
+        weather_json = weather_response.json()
+        city_info = {
+            'city': city,
+            'temp': round(weather_json['main']['temp'] - 273.15, 1),
+            'icon': weather_json['weather'][0]['icon']
+        }
+
     return render(
         request=request,
-        template_name='weather/index.html'
+        template_name='weather/index.html',
+        context={'info': city_info}
     )
