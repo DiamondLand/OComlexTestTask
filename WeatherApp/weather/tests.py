93db72351b9c361b26a3dd4d69e968e0aba0c954
Diamond Land <81316675+DiamondLand@users.noreply.github.com>
2024-07-22 13:20:29 +0300

    write tests

--- WeatherApp/weather/tests.py
+++ WeatherApp/weather/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import City
+from .forms import CityForm
+
+
+class WeatherViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('index')
+        self.city_name = 'Moscow'
+        City.objects.create(name=self.city_name)
+
+    def test_get_request(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'weather/index.html')
+        self.assertIsInstance(response.context['form'], CityForm)
+        self.assertIn('all_info', response.context)
+        self.assertGreater(len(response.context['all_info']), 0)
+
+    def test_post_request(self):
+        new_city = 'London'
+        response = self.client.post(self.url, {'name': new_city})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(City.objects.filter(name=new_city).exists())
+
+    def test_weather_info_in_template(self):
+        response = self.client.get(self.url)
+        self.assertContains(response, self.city_name)
